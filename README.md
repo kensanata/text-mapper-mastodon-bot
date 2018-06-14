114ae329657ae42ff97ff92371c202e1a70e05f5
Alex Schroeder <alex@gnu.org>
2018-06-14 23:32:53 +0200

    Initial checkin

---
+++ README.md
@@ -0,0 +1,25 @@
+How to:
+
+Create a [Mastodon Bot](https://tinysubversions.com/notes/mastodon-bot/).
+You need to save the three codes you got as follows:
+
+The *Client ID* and *Client Secret* go into the first file, each on a
+line of its own. The filename is `<account>.client`, for example
+`textmapper@botsin.space.client`.
+
+Example content:
+
+```
+1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef
+abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890
+```
+
+The *Access Token* goes to a separate file called `<account>.user`,
+for example `textmapper@botsin.space.user`..
+
+Example content:
+
+```
+7890abcdef1234567890abcdef1234567890abcdef1234567890abcdef123456
+```
+
---
+++ bot.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# Copyright (C) 2018  Alex Schroeder <alex@gnu.org>
+
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+
+from mastodon import Mastodon
+import sys
+import os.path
+import urllib.request
+import cairosvg
+
+def login(account, scopes = ['read', 'write']):
+    """
+    Login to your Mastodon account
+    """
+
+    (username, domain) = account.split("@")
+
+    url = 'https://' + domain
+    client_secret = account + '.client'
+    user_secret = account + '.user'
+    mastodon = None
+
+    if not os.path.isfile(client_secret):
+        print("Error: you need to create the file '%s'" % client_secret,
+              file=sys.stderr)
+        sys.exit(1)
+
+    if not os.path.isfile(user_secret):
+        print("Error: you need to create the file '%s'" % user_secret,
+              file=sys.stderr)
+        sys.exit(1)
+
+    mastodon = Mastodon(
+        client_id = client_secret,
+        access_token = user_secret,
+        api_base_url = url)
+
+    return mastodon
+
+def get_image(url):
+    # opener = urllib.request.FancyURLopener({})
+    # url = "https://campaignwiki.org/text-mapper/alpine/random"
+    # f = opener.open(url)
+    # return f.read()
+    return cairosvg.svg2png(url=url)
+    
+def main():
+    if len(sys.argv) == 1:
+        print("Error: you must provide an account", file=sys.stderr)
+        sys.exit(1)
+    mastodon = login(sys.argv[1])
+    url = "https://campaignwiki.org/text-mapper/alpine/random"
+    text = "#textmapper #hex #map #rpg"
+    png = get_image(url)
+    media = mastodon.media_post(png, mime_type="image/png", description="a hex map")
+    mastodon.status_post(text, media_ids=[media.id])
+
+if __name__ == "__main__":
+    main()
