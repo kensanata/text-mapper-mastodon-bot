a3d0c2e6741626100e71caeff64689588a0ff8fa
Alex Schroeder <alex@gnu.org>
2019-12-29 11:40:29 +0100

    Also generate Smale maps

--- bot.py
+++ bot.py
@@ -56,32 +56,34 @@ def login(account):
 def main(account, debug=False):
     mastodon = login(account)
     seed = random.randint(0, 2**32)
+    algorithm = random.choice(["alpine", "smale"])
     app_url = "https://campaignwiki.org/text-mapper"
     help_url = app_url + "/help"
-    svg_url = app_url + "/alpine/random"
+    svg_url = app_url + "/" + algorithm + "/random"
     args = ["seed=%d" % seed];
-    if random.random() > 0.6:
-        args.append("bottom=%d" % random.randint(1,6))
-    if random.random() > 0.6:
-        args.append("peak=%d" % random.randint(7,9))
-    if random.random() > 0.8:
-        args.append("peaks=%d" % random.randint(1,20))
-    if random.random() > 0.8:
-        args.append("steepness=%.1f" % (1 + random.randint(0,50)/10))
-    if random.random() > 0.8:
-        args.append("arid=%.1f" % (random.randint(0,20)/10))
-    if random.random() > 0.9:
-        args.append("type=square")
+    if (algorithm == "alpine"):
+        if random.random() > 0.6:
+            args.append("bottom=%d" % random.randint(1,6))
+        if random.random() > 0.6:
+            args.append("peak=%d" % random.randint(7,9))
+        if random.random() > 0.8:
+            args.append("peaks=%d" % random.randint(1,20))
+        if random.random() > 0.8:
+            args.append("steepness=%.1f" % (1 + random.randint(0,50)/10))
+        if random.random() > 0.8:
+            args.append("arid=%.1f" % (random.randint(0,20)/10))
+        if random.random() > 0.9:
+            args.append("type=square")
     svg_url += "?" + "&".join(args)
     # download SVG
     svg = urllib.request.urlopen(svg_url).read()
     # convert SVG to PNG
     png = cairosvg.svg2png(bytestring=svg)
     # create the status text
-    text = ("An Alpine map for your next mini campaign! "
+    text = ("An " + algorithm.capitalize() + " map for your next mini campaign! "
             + "Download SVG: " + svg_url + "\n"
             + "(save as PDF using Inkscape, for example)\n"
-            + "Create your own maps with the web app:" + app_url + "\n"
+            + "Create your own maps with the web app: " + app_url + "\n"
             + "How to: " + help_url + "\n"
             + "#textmapper #hex #hexcrawl #map #rpg")
     # abort now if debugging
@@ -97,6 +99,7 @@ def usage():
     print("Please provide an account name like nick@example.org")
     print("Use --debug to print the status instead of posting it")
     print("Use --help to print this message")
+    print("Set the DEBUG environment variable for preview")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
